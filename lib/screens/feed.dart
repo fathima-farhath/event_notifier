abd5eff3321ff5ffd7ee6adac1da1e6e6efe00df
Farhath <farhathali631@gmail.com>
2023-04-01 08:28:45 +0530

    Made Scaffold constant

--- lib/screens/feed.dart
+++ lib/screens/feed.dart
@@ -10,7 +10,7 @@ class MyFeed extends StatefulWidget {
 class _MyFeedState extends State<MyFeed> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       
     );
   }
