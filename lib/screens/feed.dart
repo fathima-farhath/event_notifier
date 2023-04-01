76d2f834319fc944b992e58a07b7bac9468f6d2c
Farhath <farhathali631@gmail.com>
2023-04-01 08:21:07 +0530

    Made some modifications in the project

---
+++ lib/screens/feed.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyFeed extends StatefulWidget {
+  const MyFeed({super.key});
+
+  @override
+  State<MyFeed> createState() => _MyFeedState();
+}
+
+class _MyFeedState extends State<MyFeed> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
