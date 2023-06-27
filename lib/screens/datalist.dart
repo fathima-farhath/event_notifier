aa6bf07e6a290c6adff17d2dc6141c78eae638bc
FARHANA3491 <fathimafarhana3491@gmail.com>
2023-06-27 11:52:22 +0530

    chumma commit

--- lib/screens/datalist.dart
+++ lib/screens/datalist.dart
@@ -1,5 +1,7 @@
+import 'signup.dart';
+
 var data = [
-  {"title": "Student", "image": "images/student.png"},
-  {"title": "Faculty", "image": "images/teacher.png"},
-  {"title": "Club", "image": "images/others.jfif"},
+  {"title": "Student", "image": "images/student.png", "loc": registerScreen()},
+  {"title": "Faculty", "image": "images/teacher.png", "loc": registerScreen()},
+  {"title": "Club", "image": "images/others.jfif", "loc": registerScreen()},
 ];
